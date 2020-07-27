8b1326c95a9f79627cce979739b2650e49545e9b
NoelGaspar <waaraya@uc.cl>
2020-07-27 16:02:47 -0400

    starting test. testing socket io

---
+++ Code_client/client.py
@@ -0,0 +1,35 @@
+"""
+SMVA-Server code testing
+
+
+"""
+import socketio
+import time
+
+SERVER_IP_ADDR = '192.168.0.8'
+sio = socketio.Client()
+
+
+def send_data():
+  sio.emit('my_message',{'data':cnt})
+  while cnt<1000:
+    sio.emit('my_message',{'data':cnt})
+    cnt+=1
+
+
+@sio.event
+def connect():
+    print('connection established')
+    sio.start_background_task(send_data)
+
+@sio.event
+def my_message(data):
+    print('message received with ', data)
+    sio.emit('my response', {'response': 'my response'})
+
+@sio.event
+def disconnect():
+    print('disconnected from server')
+
+sio.connect('http://'+SERVER_IP_ADDR+':5000')
+#sio.wait()
