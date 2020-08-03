a23ebdbbbc4485b3b6b2e670ce736e7480d935dc
NoelGaspar <waaraya@uc.cl>
2020-08-03 13:46:33 -0400

    fixing server side server_socket

--- Code_server/server.py
+++ Code_server/server.py
@@ -15,15 +15,15 @@ mysocket = socket.socket()
 
 print("\n Server is listing on port:",PORT,"\n")
 
-mysocket.bind(('',PORT))
-mysocket.listen(10)
+server_socket.bind(('',PORT))
+server_socket.listen(10)
 
 rcv_filename = datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")+".csv"
 rcv_file = open(rcv_filename,"a+")
 print("\n Copied file will be recived be ",rcv_filename," at server side")
 
 while True:
-  myconn,addr_conn = s.accept()
+  myconn,addr_conn = server_socket.accept()
   msg = "\n Hi Client [IP address:"+addr_conn[0]+"]"
   myconn.send(msg.encode())
 
